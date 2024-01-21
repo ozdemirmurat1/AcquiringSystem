24b5569cbeaca61d32ae66175325e44b83731f13
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-01-21 23:27:35 +0300

    Commit

---
+++ src/corePackages/Core.CrossCuttingConcerns/Logging/Serilog/Logger/MsSqlLogger.cs
@@ -0,0 +1,32 @@
+﻿using Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Logging.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Logger
+{
+    public class MsSqlLogger : LoggerServiceBase
+    {
+        private IConfiguration _configuration;
+        public MsSqlLogger(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            MsSqlConfiguration logConfiguration =
+                configuration.GetSection("SeriLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>()
+                ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+            MSSqlServerSinkOptions sinkOptions =
+                new() { TableName = logConfiguration.TableName, AutoCreateSqlTable = logConfiguration.AutoCreateSqlTable };
+
+            ColumnOptions columnOptions = new();
+
+            global::Serilog.Core.Logger serilogConfig = new LoggerConfiguration().WriteTo
+                .MSSqlServer(logConfiguration.ConnectionString, sinkOptions, columnOptions: columnOptions)
+                .CreateLogger();
+
+            Logger = serilogConfig;
+        }
+    }
+}
